a14fea3256d6268f92a9db31095143c044765036
juliusmarcel <95739349+juliusmarcel@users.noreply.github.com>
2025-05-28 14:38:05 +0700

    Update db

--- db.py
+++ db.py
@@ -1,14 +1,17 @@
 import pyodbc
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 def get_connection():
     try:
         conn = pyodbc.connect(
-            'DRIVER={ODBC Driver 17 for SQL Server};'
-            'SERVER=104.215.195.38;'  # IP kamu nanti diisi
-            'DATABASE=ShuttleBus;'
-            'UID=minipc;'       # ganti
-            'PWD=minipc2025;'       # ganti
-            
+            f"DRIVER={{{os.getenv('DB_DRIVER')}}};"
+            f"SERVER={os.getenv('DB_SERVER')};"
+            f"DATABASE={os.getenv('DB_NAME')};"
+            f"UID={os.getenv('DB_USER')};"
+            f"PWD={os.getenv('DB_PASSWORD')};"
         )
         return conn
     except pyodbc.Error as e:
